a06b29d6664cdaa59b5d010aca7467ac6895e7f7
Yaroslav Serhieiev <yaroslavs@wix.com>
2021-01-08 16:19:31 +0200

    chore: remove outdated transifex scripts

--- qfg1/scripts/import_from_transifex.sh
+++
@@ -1,3 +0,0 @@
-# curl -i -L --user api:$TRANSIFEX_TOKEN -X GET https://www.transifex.com/api/2/projects/
-# curl -i -L --user api:$TRANSIFEX_TOKEN -X GET https://api.transifex.com/organizations/old-quests-fan-translations/projects/quest-for-glory-i/resources/
-# curl -i -L --user api:$TRANSIFEX_TOKEN -X GET https://www.transifex.com/api/2/project/quest-for-glory-i/resource/10enjson/translation/he/strings/
